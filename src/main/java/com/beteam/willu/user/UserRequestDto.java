ede501cae9c10958b3b7da399fc6cd3ff1b26108
HyungCheolSim <sim34122@gmail.com>
2023-08-17 12:01:07 +0900

    :art: feat: 유저 회원가입, 로그인, 수정, 삭제, 상세 조회 service,controller 구현

---
+++ src/main/java/com/beteam/willu/user/UserRequestDto.java
@@ -0,0 +1,17 @@
+package com.beteam.willu.user;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+
+// lombok
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserRequestDto {
+
+    private String username;
+
+    private String password;
+
+    private String nickname;
+
+}
