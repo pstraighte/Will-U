0cf321dbe78d31abe6c6a13aebbcc163507fed5d
LEEMINWOO <mw7328@naver.com>
2023-08-25 09:59:00 +0900

    Feat: "채팅기능 병합 완료"
    채팅방 사용자 테이블 수정

--- src/main/java/com/beteam/willu/stomp/repository/UserChatRoomsRepository.java
+++ src/main/java/com/beteam/willu/stomp/repository/UserChatRoomsRepository.java
@@ -1,6 +1,8 @@
 package com.beteam.willu.stomp.repository;
 
+import com.beteam.willu.stomp.entity.ChatRoom;
 import com.beteam.willu.stomp.entity.UserChatRoom;
+import com.beteam.willu.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,4 +14,6 @@ public interface UserChatRoomsRepository extends JpaRepository<UserChatRoom, Lon
     Optional<UserChatRoom> findByChatRoomsIdAndUserId(Long chatid, Long userId);
 
     List<UserChatRoom> findAllByChatRoomsId(Long chatRoomId);
+
+    boolean existsByUserAndChatRooms(User user, ChatRoom chatRoom);
 }
