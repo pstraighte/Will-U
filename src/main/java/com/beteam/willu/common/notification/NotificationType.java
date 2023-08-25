3fbd30199a0f463079a419ebb8501698f2336136
HyungCheolSim <sim34122@gmail.com>
2023-08-25 12:46:10 +0900

    :sparkles: Feat: SSE 알림 구현 - Notification 구현

---
+++ src/main/java/com/beteam/willu/common/notification/NotificationType.java
@@ -0,0 +1,17 @@
+package com.beteam.willu.common.notification;
+
+public enum NotificationType {
+	JOIN_REQUEST("채팅방 참가 신청"),
+	RECRUIT_DONE("인원 모집 완료"),
+	LOGIN_DONE("로그인 완료 알림 for test");
+
+	private final String description;
+
+	NotificationType(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+}
