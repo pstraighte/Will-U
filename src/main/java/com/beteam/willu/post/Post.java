ad60c127ad3e37ceacbe91341d3fbfe904fca524
Lee SangIn <tescodanish@naver.com>
2023-08-25 13:36:48 +0900

    :memo: Add: 조회 권한 추가, Post에 username 추가
    WebSecurityConfig 에 게시글 조회, 검색을 통한 조회 권한 부여 (permitAll)

--- src/main/java/com/beteam/willu/post/Post.java
+++ src/main/java/com/beteam/willu/post/Post.java
@@ -1,13 +1,22 @@
 package com.beteam.willu.post;
 
-import com.beteam.willu.common.Timestamped;
-import com.beteam.willu.user.User;
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDateTime;
+import com.beteam.willu.common.Timestamped;
+import com.beteam.willu.user.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
@@ -15,66 +24,78 @@ import java.time.LocalDateTime;
 @Table(name = "posts")
 public class Post extends Timestamped {
 
-    /**
-     * 컬럼 - 연관관계 컬럼을 제외한 컬럼을 정의합니다.
-     */
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	/**
+	 * 컬럼 - 연관관계 컬럼을 제외한 컬럼을 정의합니다.
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(name = "post_name")
-    private String title;
+	@Column(name = "post_name")
+	private String title;
 
-    @Column(name = "post_content")
-    private String content;
+	@Column(name = "post_content")
+	private String content;
 
-    private Boolean recruitment = true;
+	private Boolean recruitment = true;
 
-    @Column(name = "post_time")
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private LocalDateTime promiseTime;
+	@Column(name = "post_time")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDateTime promiseTime;
 
-    @Column(name = "post_area")
-    private String promiseArea;
+	@Column(name = "post_area")
+	private String promiseArea;
 
-    @Column(name = "post_maxnum")
-    private Long maxnum; //모집 최대 인원
-    /**
-     * 연관관계 - Foreign Key 값을 따로 컬럼으로 정의하지 않고 연관 관계로 정의합니다.
-     */
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+	@Column(name = "post_maxnum")
+	private Long maxnum; //모집 최대 인원
+	/**
+	 * 연관관계 - Foreign Key 값을 따로 컬럼으로 정의하지 않고 연관 관계로 정의합니다.
+	 */
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 
-    /**
-     * 생성자 - 약속된 형태로만 생성가능하도록 합니다.
-     */
-    public Post(PostRequestDto postRequestDto) {
-        if (postRequestDto.getTitle() != null) this.title = postRequestDto.getTitle();
-        if (postRequestDto.getContent() != null) this.content = postRequestDto.getContent();
-        if (postRequestDto.getPromiseTime() != null) this.promiseTime = postRequestDto.getPromiseTime();
-        if (postRequestDto.getPromiseArea() != null) this.promiseArea = postRequestDto.getPromiseArea();
-        if (postRequestDto.getMaxnum() != null) this.maxnum = postRequestDto.getMaxnum();
-    }
+	private String username;
 
-    /**
-     * 연관관계 편의 메소드 - 반대쪽에는 연관관계 편의 메소드가 없도록 주의합니다.
-     */
-    /**
-     * 서비스 메소드 - 외부에서 엔티티를 수정할 메소드를 정의합니다. (단일 책임을 가지도록 주의합니다.)
-     */
-    public void setUser(User user) {
-        this.user = user;
-    }
+	/**
+	 * 생성자 - 약속된 형태로만 생성가능하도록 합니다.
+	 */
+	public Post(PostRequestDto postRequestDto, User user) {
+		this.title = postRequestDto.getTitle();
+		if (postRequestDto.getContent() != null)
+			this.content = postRequestDto.getContent();
+		if (postRequestDto.getPromiseTime() != null)
+			this.promiseTime = postRequestDto.getPromiseTime();
+		if (postRequestDto.getPromiseArea() != null)
+			this.promiseArea = postRequestDto.getPromiseArea();
+		if (postRequestDto.getMaxnum() != null)
+			this.maxnum = postRequestDto.getMaxnum();
+		this.user = user;
+		this.username = user.getUsername();
+	}
 
-    public void setRecruitment(Boolean recruitment) {
-        this.recruitment = recruitment;
-    }
+	/**
+	 * 연관관계 편의 메소드 - 반대쪽에는 연관관계 편의 메소드가 없도록 주의합니다.
+	 */
+	/**
+	 * 서비스 메소드 - 외부에서 엔티티를 수정할 메소드를 정의합니다. (단일 책임을 가지도록 주의합니다.)
+	 */
+	public void setUser(User user) {
+		this.user = user;
+	}
 
-    public void update(PostRequestDto postRequestDto) {
-        if (postRequestDto.getTitle() != null) this.title = postRequestDto.getTitle();
-        if (postRequestDto.getContent() != null) this.content = postRequestDto.getContent();
-        if (postRequestDto.getPromiseTime() != null) this.promiseTime = postRequestDto.getPromiseTime();
-        if (postRequestDto.getPromiseArea() != null) this.promiseArea = postRequestDto.getPromiseArea();
-    }
+	public void setRecruitment(Boolean recruitment) {
+		this.recruitment = recruitment;
+	}
+
+	public void update(PostRequestDto postRequestDto) {
+		if (postRequestDto.getTitle() != null)
+			this.title = postRequestDto.getTitle();
+		if (postRequestDto.getContent() != null)
+			this.content = postRequestDto.getContent();
+		if (postRequestDto.getPromiseTime() != null)
+			this.promiseTime = postRequestDto.getPromiseTime();
+		if (postRequestDto.getPromiseArea() != null)
+			this.promiseArea = postRequestDto.getPromiseArea();
+	}
 }
