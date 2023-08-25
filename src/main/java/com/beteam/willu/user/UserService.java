9b7904b778e638e743e77b06f72da88e931badf2
HyungCheolSim <sim34122@gmail.com>
2023-08-25 12:48:56 +0900

    :sparkles: Feat: SSE 알림 구현
    Service: 실제 알림이 필요한 곳에 메서드를 만들고 넣는다.
    Entity: eventpublisher 호출(transaction의 중심)

--- src/main/java/com/beteam/willu/user/UserService.java
+++ src/main/java/com/beteam/willu/user/UserService.java
@@ -1,169 +1,192 @@
 package com.beteam.willu.user;
 
-import com.beteam.willu.common.jwt.JwtUtil;
-import com.beteam.willu.common.redis.RedisUtil;
-import com.beteam.willu.common.security.UserDetailsImpl;
-import com.sun.jdi.request.DuplicateRequestException;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.Optional;
+import com.beteam.willu.common.jwt.JwtUtil;
+import com.beteam.willu.common.notification.NotificationType;
+import com.beteam.willu.common.redis.RedisUtil;
+import com.beteam.willu.common.security.UserDetailsImpl;
+import com.sun.jdi.request.DuplicateRequestException;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j(topic = "userService")
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    private final UserRepository userRepository;
-    private final InterestRepository interestRepository;
-    private final BlacklistRepository blacklistRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtUtil jwtUtil;
-    private final RedisUtil redisUtil;
+	private final UserRepository userRepository;
+	private final InterestRepository interestRepository;
+	private final BlacklistRepository blacklistRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final JwtUtil jwtUtil;
+	private final RedisUtil redisUtil;
+	private final ApplicationEventPublisher eventPublisher;
 
-    public void userSignup(UserRequestDto requestDto) {
+	public void userSignup(UserRequestDto requestDto) {
 
-        if (userRepository.findByUsername(requestDto.getUsername()).isPresent()) {
-            throw new IllegalArgumentException("해당 유저가 이미 있습니다.");
-        }
+		if (userRepository.findByUsername(requestDto.getUsername()).isPresent()) {
+			throw new IllegalArgumentException("해당 유저가 이미 있습니다.");
+		}
 
-        if (userRepository.findByNickname(requestDto.getNickname()).isPresent()) {
-            throw new IllegalArgumentException("중복된 username 입니다");
-        }
+		if (userRepository.findByNickname(requestDto.getNickname()).isPresent()) {
+			throw new IllegalArgumentException("중복된 username 입니다");
+		}
 
-        // 카카오 계정으로 사용된 email로 또 회원가입을 진행 할시
-        // 또는 중복된 email이 있는지 확인
-        if (userRepository.findByEmail(requestDto.getEmail()).isPresent()) {
-            throw new IllegalArgumentException("중복된 email 입니다");
-        }
+		// 카카오 계정으로 사용된 email로 또 회원가입을 진행 할시
+		// 또는 중복된 email이 있는지 확인
+		if (userRepository.findByEmail(requestDto.getEmail()).isPresent()) {
+			throw new IllegalArgumentException("중복된 email 입니다");
+		}
 
-        String password = passwordEncoder.encode(requestDto.getPassword());
+		String password = passwordEncoder.encode(requestDto.getPassword());
 
-        User user = User.builder().username(requestDto.getUsername()).password(password).nickname(requestDto.getNickname()).email(requestDto.getEmail()).build();
+		User user = User.builder()
+			.username(requestDto.getUsername())
+			.password(password)
+			.nickname(requestDto.getNickname())
+			.email(requestDto.getEmail())
+			.build();
 
-        userRepository.save(user);
-    }
+		userRepository.save(user);
+	}
 
-    public void userLogin(UserRequestDto requestDto, HttpServletResponse response) {
-        log.info("userService login 진입");
-        String username = requestDto.getUsername();
-        User user = findUser(username);
+	@Transactional
+	public void userLogin(UserRequestDto requestDto, HttpServletResponse response) {
+		log.info("userService login 진입");
+		String username = requestDto.getUsername();
+		User user = findUser(username);
 
-        if (!passwordEncoder.matches(requestDto.getPassword(), user.getPassword())) {
-            throw new IllegalArgumentException("로그인 실패 비밀번호 틀립니다!");
-        }
+		if (!passwordEncoder.matches(requestDto.getPassword(), user.getPassword())) {
+			throw new IllegalArgumentException("로그인 실패 비밀번호 틀립니다!");
+		}
 
-        //토큰 생성
-        String accessToken = jwtUtil.createAccessToken(username);
-        String refreshToken = jwtUtil.createRefreshToken(username);
+		//토큰 생성
+		String accessToken = jwtUtil.createAccessToken(username);
+		String refreshToken = jwtUtil.createRefreshToken(username);
 
-        log.info("refresh token: " + refreshToken);
-        log.info("access token: " + accessToken);
-        // refreshToken redis 저장
-        redisUtil.saveRefreshToken(username, refreshToken);
+		log.info("refresh token: " + refreshToken);
+		log.info("access token: " + accessToken);
+		// refreshToken redis 저장
+		redisUtil.saveRefreshToken(username, refreshToken);
 
-        // accessToken, refreshToken cookie 저장
-        jwtUtil.addJwtToCookie(accessToken, JwtUtil.AUTHORIZATION_HEADER, response);
-        jwtUtil.addJwtToCookie(refreshToken, JwtUtil.REFRESH_TOKEN_HEADER, response);
+		// accessToken, refreshToken cookie 저장
+		jwtUtil.addJwtToCookie(accessToken, JwtUtil.AUTHORIZATION_HEADER, response);
+		jwtUtil.addJwtToCookie(refreshToken, JwtUtil.REFRESH_TOKEN_HEADER, response);
+	}
 
-    }
+	private void notifyLoginInfo(User user, NotificationType type) {
+		user.publishEvent(eventPublisher, type);
+	}
 
-    public void logout(String accessToken, HttpServletResponse response, String username) {
-        //쿠키에서 가져온 토큰추출
-        accessToken = URLDecoder.decode(accessToken, StandardCharsets.UTF_8).substring(7);
-        log.info("accessToken 값: " + accessToken);
+	public void logout(String accessToken, HttpServletResponse response, String username) {
+		//쿠키에서 가져온 토큰추출
+		accessToken = URLDecoder.decode(accessToken, StandardCharsets.UTF_8).substring(7);
+		log.info("accessToken 값: " + accessToken);
 
-        if (redisUtil.getRefreshToken(username) != null) {
-            log.info("로그아웃 시 리프레시 토큰이 존재하면 지워준다.");
-            redisUtil.deleteRefreshToken(username);
-        }
-        log.info("액세스 토큰 블랙리스트로 저장 : " + accessToken);
-        redisUtil.addBlackList(accessToken, jwtUtil.getExpiration(accessToken));
-        //쿠키 삭제
-        jwtUtil.expireCookie(response, JwtUtil.AUTHORIZATION_HEADER);
-        jwtUtil.expireCookie(response, JwtUtil.REFRESH_TOKEN_HEADER);
-    }
+		if (redisUtil.getRefreshToken(username) != null) {
+			log.info("로그아웃 시 리프레시 토큰이 존재하면 지워준다.");
+			redisUtil.deleteRefreshToken(username);
+		}
+		log.info("액세스 토큰 블랙리스트로 저장 : " + accessToken);
+		redisUtil.addBlackList(accessToken, jwtUtil.getExpiration(accessToken));
+		//쿠키 삭제
+		jwtUtil.expireCookie(response, JwtUtil.AUTHORIZATION_HEADER);
+		jwtUtil.expireCookie(response, JwtUtil.REFRESH_TOKEN_HEADER);
+	}
 
-    // 유저 조회 (프로파일)
-    public UserResponseDto getProfile(Long id) {
-        User user = findUser(id);
-        return new UserResponseDto(user);
-    }
+	// 유저 조회 (프로파일)
+	public UserResponseDto getProfile(Long id) {
+		User user = findUser(id);
+		return new UserResponseDto(user);
+	}
 
-    // 유저 업데이트 (프로파일)
-    @Transactional
-    public UserResponseDto userUpdate(UserUpdateRequestDto updateRequestDto, UserDetailsImpl users) {
-        User user = users.getUser();
-        user.profileUpdate(updateRequestDto);
-        return new UserResponseDto(user);
-    }
+	// 유저 업데이트 (프로파일)
+	@Transactional
+	public UserResponseDto userUpdate(UserUpdateRequestDto updateRequestDto, UserDetailsImpl users) {
+		User user = users.getUser();
+		user.profileUpdate(updateRequestDto);
+		return new UserResponseDto(user);
+	}
 
-    @Transactional
-    public void deleteUser(Long id, User user) {
-        if (!id.equals(user.getId()))
-            throw new IllegalArgumentException("본인이 아닙니다. 탈퇴할 수 없습니다.");
+	@Transactional
+	public void deleteUser(Long id, User user) {
+		if (!id.equals(user.getId()))
+			throw new IllegalArgumentException("본인이 아닙니다. 탈퇴할 수 없습니다.");
 
-        userRepository.delete(user);
-    }
+		userRepository.delete(user);
+	}
 
-    @Transactional  //관심유저 추가
-    public void addInterest(Long id, User user) {
-        User receiver = findUser(id);
+	@Transactional  //관심유저 추가
+	public void addInterest(Long id, User user) {
+		User receiver = findUser(id);
 
-        if (interestRepository.existsByReceiverIdAndSenderId(receiver.getId(), user.getId())) {
-            throw new DuplicateRequestException("이미 관심등록 된 유저 입니다.");
-        } else {
-            Interest interest= new Interest(receiver, user);
-            interestRepository.save(interest);
-        }
-    }
-    @Transactional
-    public void removeInterest(Long id, User user) {
-        User receiver = findUser(id);
+		if (interestRepository.existsByReceiverIdAndSenderId(receiver.getId(), user.getId())) {
+			throw new DuplicateRequestException("이미 관심등록 된 유저 입니다.");
+		} else {
+			Interest interest = new Interest(receiver, user);
+			interestRepository.save(interest);
+			notifyLoginInfo(user, NotificationType.LOGIN_DONE);
+		}
+	}
 
-        Optional<Interest> interestOptional = interestRepository.findByReceiverIdAndSenderId(receiver.getId(), user.getId());
-        if (interestOptional.isPresent()) {
-            interestRepository.delete(interestOptional.get());
-        } else {
-            throw new IllegalArgumentException("이미 관심해제 된 유저 입니다.");
-        }
-    }
-    @Transactional //차단 유저 추가
-    public void addBlacklist(Long id, User user) {
-        User receiver = findUser(id);
+	@Transactional
+	public void removeInterest(Long id, User user) {
+		User receiver = findUser(id);
 
-        if (blacklistRepository.existsByReceiverIdAndSenderId(receiver.getId(), user.getId())) {
-            throw new DuplicateRequestException("이미 차단된 유저 입니다.");
-        } else {
-            Blacklist blacklist= new Blacklist(receiver, user);
-            blacklistRepository.save(blacklist);
-        }
-    }
+		Optional<Interest> interestOptional = interestRepository.findByReceiverIdAndSenderId(receiver.getId(),
+			user.getId());
+		if (interestOptional.isPresent()) {
+			interestRepository.delete(interestOptional.get());
+		} else {
+			throw new IllegalArgumentException("이미 관심해제 된 유저 입니다.");
+		}
+	}
 
-    @Transactional  //차단 유저 해제
-    public void removeBlacklist(Long id, User user) {
-        User receiver = findUser(id);
+	@Transactional //차단 유저 추가
+	public void addBlacklist(Long id, User user) {
+		User receiver = findUser(id);
 
-        Optional<Blacklist> blacklistOptional = blacklistRepository.findByReceiverIdAndSenderId(receiver.getId(), user.getId());
-        if (blacklistOptional.isPresent()) {
-            blacklistRepository.delete(blacklistOptional.get());
-        } else {
-            throw new IllegalArgumentException("이미 차단해제 된 유저 입니다.");
-        }
+		if (blacklistRepository.existsByReceiverIdAndSenderId(receiver.getId(), user.getId())) {
+			throw new DuplicateRequestException("이미 차단된 유저 입니다.");
+		} else {
+			Blacklist blacklist = new Blacklist(receiver, user);
+			blacklistRepository.save(blacklist);
+		}
+		//sse 테스트용
+		notifyLoginInfo(user, NotificationType.LOGIN_DONE);
+	}
 
-    }
-    private User findUser(String username) {
-        return userRepository.findByUsername(username).orElseThrow(() -> new IllegalArgumentException("유저가 존재하지 않습니다."));
-    }
+	@Transactional  //차단 유저 해제
+	public void removeBlacklist(Long id, User user) {
+		User receiver = findUser(id);
 
-    private User findUser(Long id) {
-        return userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("유저가 존재하지 않습니다."));
-    }
+		Optional<Blacklist> blacklistOptional = blacklistRepository.findByReceiverIdAndSenderId(receiver.getId(),
+			user.getId());
+		if (blacklistOptional.isPresent()) {
+			blacklistRepository.delete(blacklistOptional.get());
+		} else {
+			throw new IllegalArgumentException("이미 차단해제 된 유저 입니다.");
+		}
+
+	}
+
+	private User findUser(String username) {
+		return userRepository.findByUsername(username)
+			.orElseThrow(() -> new IllegalArgumentException("유저가 존재하지 않습니다."));
+	}
+
+	private User findUser(Long id) {
+		return userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("유저가 존재하지 않습니다."));
+	}
 
 }
