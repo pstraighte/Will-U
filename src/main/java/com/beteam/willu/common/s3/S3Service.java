0b6d0b547edaef5df534e36dc3cfa60c168c2754
Lee SangIn <tescodanish@naver.com>
2023-09-01 09:47:15 +0900

    :art: Modified: S3 관련 변경
    Controller
    uploadImage를 통해 단순히 이미지를 S3에 업로드하는 것 뿐 아닌 업데이트를 반영하도록 수정
    User정보 받아오는 내용 추가
    
    Service
    버킷 이름 받아오는 이름 수정
    업로드 시 return되는 objectKey를 사용해 객체 url을 생성해주는 generateImageUrl 메서드를 사용하여 객체 url 생성 및 유저에 저장

--- src/main/java/com/beteam/willu/common/s3/S3Service.java
+++ src/main/java/com/beteam/willu/common/s3/S3Service.java
@@ -8,16 +8,23 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.beteam.willu.user.entity.User;
+import com.beteam.willu.user.repository.UserRepository;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class S3Service {
 
 	private final AmazonS3 s3Client; // AmazonS3 클라이언트 주입
 
-	private static final String BUCKET_NAME = "willu-image"; // 생성한 버킷 이름
+	private static final String BUCKET_NAME = "willu-bucket"; // 생성한 버킷 이름
+	private final UserRepository userRepository;
 
-	public S3Service(AmazonS3 s3Client) {
+	public S3Service(AmazonS3 s3Client, UserRepository userRepository) {
 		this.s3Client = s3Client; // AmazonS3 클라이언트 주입
+		this.userRepository = userRepository;
 	}
 
 	public String uploadImage(MultipartFile imageFile) throws IOException {
@@ -29,6 +36,13 @@ public class S3Service {
 
 		s3Client.putObject(BUCKET_NAME, objectKey, imageFile.getInputStream(), metadata);  // S3에 이미지 업로드
 
+		log.info(objectKey);
 		return objectKey; // 업로드된 이미지의 S3 오브젝트 키를 반환
+	}
+
+	public void generateImageUrl(String objectKey, User user) {
+		String bucketBaseUrl = "https://willu-bucket.s3.ap-northeast-2.amazonaws.com"; // Replace with your bucket URL
+		user.setPicture(bucketBaseUrl + "/" + objectKey);
+		userRepository.save(user);
 	}
 }
