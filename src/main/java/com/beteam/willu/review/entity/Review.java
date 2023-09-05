8873521b2724c02b26eaa369987e7d2c8f73b4f9
Lee SangIn <tescodanish@naver.com>
2023-09-05 22:25:29 +0900

    :art: Modify: score 타입 변경 long->Double
    PostResponseDto
    ReviewRequestDto
    UserResponseDto
    User
    ReviewSetResponseDto
    Review

--- src/main/java/com/beteam/willu/review/entity/Review.java
+++ src/main/java/com/beteam/willu/review/entity/Review.java
@@ -1,12 +1,20 @@
 package com.beteam.willu.review.entity;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.beteam.willu.common.Timestamped;
 import com.beteam.willu.stomp.entity.ChatRoom;
 import com.beteam.willu.user.entity.User;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.transaction.annotation.Transactional;
 
 @Entity
 @Getter
@@ -14,37 +22,37 @@ import org.springframework.transaction.annotation.Transactional;
 @NoArgsConstructor
 public class Review extends Timestamped {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "receiver_id") //리뷰 수신자-admin
-    private User receiver;
+	@ManyToOne
+	@JoinColumn(name = "receiver_id") //리뷰 수신자-admin
+	private User receiver;
 
-    @ManyToOne
-    @JoinColumn(name = "sender_id") //리뷰 작성자-게스트
-    private User sender;
+	@ManyToOne
+	@JoinColumn(name = "sender_id") //리뷰 작성자-게스트
+	private User sender;
 
-    @ManyToOne
-    @JoinColumn(name = "chatRoom_id")
-    private ChatRoom chatRoom;
+	@ManyToOne
+	@JoinColumn(name = "chatRoom_id")
+	private ChatRoom chatRoom;
 
-    private String content;
+	private String content;
 
-    private long score;
+	private Double score;
 
-    public Review(User receiver, User sender, ChatRoom userChatRoom, String content, long score) {
-        this.receiver = receiver;
-        this.sender = sender;
-        this.chatRoom = userChatRoom;
-        this.content = content;
-        this.score = score;
-    }
+	public Review(User receiver, User sender, ChatRoom userChatRoom, String content, Double score) {
+		this.receiver = receiver;
+		this.sender = sender;
+		this.chatRoom = userChatRoom;
+		this.content = content;
+		this.score = score;
+	}
 
-    @Transactional
-    public void updateReview(String content, long score) {
-        this.content = content;
-        this.score = score;
-    }
+	@Transactional
+	public void updateReview(String content, Double score) {
+		this.content = content;
+		this.score = score;
+	}
 }
