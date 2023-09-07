5d2a3c792bdb4312c106af38cf14a2cb293199e4
LEEMINWOO <mw7328@naver.com>
2023-09-07 22:43:42 +0900

    Feat: "JobCleanup 주석 추가"
    로식 설명 추가

--- src/main/java/com/beteam/willu/common/batch/JobCleanupService.java
+++ src/main/java/com/beteam/willu/common/batch/JobCleanupService.java
@@ -22,9 +22,11 @@ public class JobCleanupService {
     @Transactional
     public void cleanupOldJobData() {
 
+        //  "job"이라는 작업 이름을 가진 모든 작업 인스턴스를 JobExplorer를 통해 가져와서 jobInstances 리스트에 저장
         List<JobInstance> jobInstances = jobExplorer.getJobInstances("job", 0, Integer.MAX_VALUE);
 
         for (JobInstance jobInstance : jobInstances) {
+            // JobInstance에 대한 실행 이력들을 JobExplorer를 통해 검색하여 jobExecutions 리스트에 저장하는 것
             List<JobExecution> jobExecutions = jobExplorer.getJobExecutions(jobInstance);
 
             for (JobExecution jobExecution : jobExecutions) {
