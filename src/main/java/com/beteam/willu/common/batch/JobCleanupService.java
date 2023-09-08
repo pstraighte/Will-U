d2ee922be7cefac844a829bd9848172eaeca2398
HyungCheolSim <sim34122@gmail.com>
2023-09-08 12:48:47 +0900

    :recycle: 검색 시 버튼 재생성 확인, batch 파일 관리 변경

--- src/main/java/com/beteam/willu/common/batch/JobCleanupService.java
+++ src/main/java/com/beteam/willu/common/batch/JobCleanupService.java
@@ -1,5 +1,7 @@
 package com.beteam.willu.common.batch;
 
+import java.util.List;
+
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobInstance;
 import org.springframework.batch.core.explore.JobExplorer;
@@ -8,35 +10,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 public class JobCleanupService {
 
-    @Autowired
-    private JobExplorer jobExplorer;
+	@Autowired
+	private JobExplorer jobExplorer;
 
-    @Autowired
-    private JobRepository jobRepository;
+	@Autowired
+	private JobRepository jobRepository;
 
-    @Transactional
-    public void cleanupOldJobData() {
+	@Transactional
+	public void cleanupOldJobData() {
 
-        //  "job"이라는 작업 이름을 가진 모든 작업 인스턴스를 JobExplorer를 통해 가져와서 jobInstances 리스트에 저장
-        List<JobInstance> jobInstances = jobExplorer.getJobInstances("job", 0, Integer.MAX_VALUE);
+		//  "job"이라는 작업 이름을 가진 모든 작업 인스턴스를 JobExplorer를 통해 가져와서 jobInstances 리스트에 저장
+		List<JobInstance> jobInstances = jobExplorer.getJobInstances("job", 0, Integer.MAX_VALUE);
 
-        for (JobInstance jobInstance : jobInstances) {
-            // JobInstance에 대한 실행 이력들을 JobExplorer를 통해 검색하여 jobExecutions 리스트에 저장하는 것
-            List<JobExecution> jobExecutions = jobExplorer.getJobExecutions(jobInstance);
+		for (JobInstance jobInstance : jobInstances) {
+			// JobInstance에 대한 실행 이력들을 JobExplorer를 통해 검색하여 jobExecutions 리스트에 저장하는 것
+			List<JobExecution> jobExecutions = jobExplorer.getJobExecutions(jobInstance);
+			System.out.println("작업 사이즈: " + jobExecutions.size());
+			for (JobExecution jobExecution : jobExecutions) {
 
-            for (JobExecution jobExecution : jobExecutions) {
-                System.out.println("jobExecution = " + jobExecution.getEndTime());
-                if (jobExecution.getEndTime() != null) {
-                    jobRepository.deleteJobExecution(jobExecution);
-                }
-
-            }
-        }
-    }
+				if (jobExecution.getExitStatus().toString().equals("COMPLETED")) {
+					System.out.println("jobExecution DELETION = " + jobExecution.getEndTime());
+					jobRepository.deleteJobExecution(jobExecution);
+					jobRepository.deleteJobInstance(jobInstance);
+				}
+			}
+		}
+	}
 
 }
--- src/main/java/com/beteam/willu/common/batch/testTasklet.java
+++ src/main/java/com/beteam/willu/common/batch/testTasklet.java
@@ -1,11 +1,10 @@
 package com.beteam.willu.common.batch;
 
-import com.beteam.willu.post.entity.Post;
-import com.beteam.willu.post.repository.PostRepository;
-import com.beteam.willu.stomp.entity.ChatRoom;
-import com.beteam.willu.stomp.repository.ChatRoomRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
@@ -13,75 +12,74 @@ import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.util.List;
+import com.beteam.willu.post.entity.Post;
+import com.beteam.willu.post.repository.PostRepository;
+import com.beteam.willu.stomp.entity.ChatRoom;
+import com.beteam.willu.stomp.repository.ChatRoomRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class testTasklet implements Tasklet {
-    private final PostRepository postRepository;
-    private final ChatRoomRepository chatRoomRepository;
-    private final JobRepository jobRepository;
-    private final JobCleanupService jobCleanupService;
+	private final PostRepository postRepository;
+	private final ChatRoomRepository chatRoomRepository;
+	private final JobRepository jobRepository;
+	private final JobCleanupService jobCleanupService;
 
-    @Override
-//    @Transactional
-    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-        log.info("=====Start execute======");
+	@Override
+	//    @Transactional
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+		log.info("=====Start execute======");
 
-        // 생성된 job 데이터 삭제
-        jobCleanupService.cleanupOldJobData();
+		// 생성된 job 데이터 삭제
+		jobCleanupService.cleanupOldJobData();
 
-        // 활성화 되어있는 게시글을 가져온다.
-        List<Post> truePostList = postRepository.findAllByRecruitment(true);
+		// 활성화 되어있는 게시글을 가져온다.
+		List<Post> truePostList = postRepository.findAllByRecruitment(true);
 
-        // 현재 시간 데이터를 생성
-        LocalDateTime dateTime;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-        String startTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-        dateTime = LocalDateTime.parse(startTime, formatter);
+		// 현재 시간 데이터를 생성
+		LocalDateTime dateTime;
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		String startTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+		dateTime = LocalDateTime.parse(startTime, formatter);
 
-        // 시간이 지난 게시글은 비활성화로 변경
-        for (Post truePost : truePostList) {
-            if (truePost.getPromiseTime().isBefore(dateTime)) {
-                // 마감 시간이 지난 게시글
-                if (truePost.getRecruitment() == true) { // 게시글의 활성화 필드가 true 인지 확인
-                    truePost.updateRecruitment(false);
-                }
-            }
-        }
+		// 시간이 지난 게시글은 비활성화로 변경
+		for (Post truePost : truePostList) {
+			if (truePost.getPromiseTime().isBefore(dateTime)) {
+				// 마감 시간이 지난 게시글
+				if (truePost.getRecruitment()) { // 게시글의 활성화 필드가 true 인지 확인
+					System.out.println("게시물 false로 변경");
+					truePost.updateRecruitment(false);
+				}
+			}
+		}
 
-        // 비활성화 되어있는 게시글을 가져온다.
-        List<Post> falsePostList = postRepository.findAllByRecruitment(false);
+		// 비활성화 되어있는 게시글을 가져온다.
+		List<Post> falsePostList = postRepository.findAllByRecruitment(false);
 
-        // 활성화 되어있는 채팅방을 가져온다.
-        List<ChatRoom> chatRoomList = chatRoomRepository.findAllByActivated(true);
+		// 활성화 되어있는 채팅방을 가져온다.
+		//List<ChatRoom> chatRoomList = chatRoomRepository.findAllByActivated(true);
 
-        // false 로 설정된 게시글의 채팅방이 모집완료 후 1시간이 지났는지 확인
-        for (Post falsePost : falsePostList) {
+		// false 로 설정된 게시글의 채팅방이 모집완료 후 1시간이 지났는지 확인
+		for (Post falsePost : falsePostList) {
+			ChatRoom chatRoom = falsePost.getChatRoom();
+			// 비활성화 된 게시글의 모집완료 시간의 1시간 후 데이터 생성
+			LocalDateTime falsePostTime = falsePost.getPromiseTime();
+			LocalDateTime falsePostTimeAfter = falsePostTime.plus(2, ChronoUnit.MINUTES);
 
-            for (ChatRoom chatRoom : chatRoomList) {
-                // 비활성화 된 게시글의 모집완료 시간의 1시간 후 데이터 생성
-                LocalDateTime falsePostTime = falsePost.getPromiseTime();
-                LocalDateTime falsePostTimeAfter = falsePostTime.plus(2, ChronoUnit.MINUTES);
-
-                // 1. false 로 설정된 게시글의 id와 같은 채팅방
-                // 2. 채팅방 생성 기준으로 1시간이 지난 채팅방 (테스트 2분)
-                if ((chatRoom.getId() == falsePost.getId()) && falsePostTimeAfter.isBefore(dateTime)) {
-                    // 해당 채팅방의 활성화 필드를 false 로 변경
-                    if (chatRoom.isActivated()) {  // 채팅방의 활성화 필드가 true 인지 확인
-                        chatRoom.updateActivated(false);
-                    }
-
-                }
-            }
-
-        }
-
-        return RepeatStatus.FINISHED;
-    }
-
+			// 1. false 로 설정된 게시글의 id와 같은 채팅방
+			// 2. 채팅방 생성 기준으로 1시간이 지난 채팅방 (테스트 2분)
+			if (falsePostTimeAfter.isBefore(dateTime)) {
+				// 해당 채팅방의 활성화 필드를 false 로 변경
+				if (chatRoom.isActivated()) {  // 채팅방의 활성화 필드가 true 인지 확인
+					System.out.println("채팅방 false로 변경");
+					chatRoom.updateActivated(false);
+				}
+			}
+		}
+		return RepeatStatus.FINISHED;
+	}
 }
