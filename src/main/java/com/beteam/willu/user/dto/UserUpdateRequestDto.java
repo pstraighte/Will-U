3b4c84d6140a198c3f5ab9edcfb16f7a901299ac
Lee SangIn <tescodanish@naver.com>
2023-09-11 23:36:23 +0900

    Modify: front 수정
    profile.html
    - th 방식으로 user를 가져와서 username 사용
    
    detailPost.html
    - th 방식으로 user를 가져와서 user 정보 사용
    
    User
    - phoneNumber, area 필드 삭제
    
    UserResponseDto
    - phoneNumber, area 필드 삭제
    - id 추가
    
    UserUpdateRequestDto
    - phoneNumber, area, username 필드 삭제
    
    ViewController
    - 게시글 단건 조회시 user정보를 Dto에 담아서 model로 넘김
    - userScore를 반올림해서 가져오던 부분 제거

--- src/main/java/com/beteam/willu/user/dto/UserUpdateRequestDto.java
+++ src/main/java/com/beteam/willu/user/dto/UserUpdateRequestDto.java
@@ -6,13 +6,10 @@ import lombok.Getter;
 
 @Getter
 public class UserUpdateRequestDto {
-    private String username;
 
-    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
-    private String nickname;
-    
-    private String phoneNumber;
-    private String area;
-    private String picture;
+	@NotBlank(message = "닉네임은 필수 입력 값입니다.")
+	@Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
+	private String nickname;
+
+	private String picture;
 }
