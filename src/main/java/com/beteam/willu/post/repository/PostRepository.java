3eb025a1a05547f087c0c8c201f080b1302f6936
Lee SangIn <tescodanish@naver.com>
2023-09-15 01:37:54 +0900

    :sparkles: Modify: 검색기능 QueryMethod -> QueryDSL
    PostController
    - 검색조건 주석 닉네임으로 변경
    
    build.gradle
    - DSL 의존성 추가
    
    검색을 위한 PostExperssions 추가
    
    Repository
    - 기존 검색 메서드 삭제
    - findAllByOrderByIdDesc 메서드 추가
    
    PostServiceImpl
    - searchPosts 메서드 변경

--- src/main/java/com/beteam/willu/post/repository/PostRepository.java
+++ src/main/java/com/beteam/willu/post/repository/PostRepository.java
@@ -1,35 +1,22 @@
 package com.beteam.willu.post.repository;
 
-import com.beteam.willu.post.entity.Post;
-import com.beteam.willu.user.entity.User;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-import java.util.List;
+import com.beteam.willu.post.entity.Post;
+import com.beteam.willu.user.entity.User;
+import com.querydsl.core.types.Predicate;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
-    Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
+public interface PostRepository extends JpaRepository<Post, Long>, QuerydslPredicateExecutor<Post> {
+	Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
 
-    // 제목으로 검색
-    Page<Post> findByTitleContainingOrderByCreatedAtDesc(String keyword, Pageable pageable);
+	Page<Post> findAllByOrderByIdDesc(Predicate predicate, Pageable pageable);
 
-    // 작성자의 username으로 검색
-    Page<Post> findByUser_UsernameContainingOrderByCreatedAtDesc(String keyword, Pageable pageable);
+	List<Post> findAllByRecruitmentOrderByCreatedAtDesc(boolean b);
 
-    // 내용으로 검색
-    Page<Post> findByContentContainingOrderByCreatedAtDesc(String keyword, Pageable pageable);
-
-    // 제목으로 검색하고 모집 상태가 true인 게시글만 검색
-    Page<Post> findByTitleContainingAndRecruitmentIsTrueOrderByCreatedAtDesc(String keyword, Pageable pageable);
-
-    // 작성자의 username으로 검색하고 모집 상태가 true인 게시글만 검색
-    Page<Post> findByUser_UsernameContainingAndRecruitmentIsTrueOrderByCreatedAtDesc(String keyword, Pageable pageable);
-
-    // 내용으로 검색하고 모집 상태가 true인 게시글만 검색
-    Page<Post> findByContentContainingAndRecruitmentIsTrueOrderByCreatedAtDesc(String keyword, Pageable pageable);
-
-    List<Post> findAllByRecruitmentOrderByCreatedAtDesc(boolean b);
-
-    List<Post> findByUserOrderByCreatedAtDesc(User user);
+	List<Post> findByUserOrderByCreatedAtDesc(User user);
 }
--- src/main/java/com/beteam/willu/post/service/PostServiceImpl.java
+++ src/main/java/com/beteam/willu/post/service/PostServiceImpl.java
@@ -1,5 +1,13 @@
 package com.beteam.willu.post.service;
 
+import java.util.Objects;
+import java.util.concurrent.RejectedExecutionException;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.beteam.willu.common.exception.RecruitmentStatusException;
 import com.beteam.willu.hashtag.entity.BoardTagMap;
 import com.beteam.willu.hashtag.entity.Tag;
@@ -8,209 +16,214 @@ import com.beteam.willu.hashtag.repository.HashTagRepository;
 import com.beteam.willu.post.dto.PostRequestDto;
 import com.beteam.willu.post.dto.PostResponseDto;
 import com.beteam.willu.post.entity.Post;
+import com.beteam.willu.post.querydsl.PostExpressions;
 import com.beteam.willu.post.repository.PostRepository;
 import com.beteam.willu.stomp.entity.ChatRoom;
 import com.beteam.willu.stomp.entity.UserChatRoom;
 import com.beteam.willu.stomp.repository.ChatRoomRepository;
 import com.beteam.willu.stomp.repository.UserChatRoomsRepository;
 import com.beteam.willu.user.entity.User;
+import com.querydsl.core.types.Predicate;
+
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Objects;
-import java.util.concurrent.RejectedExecutionException;
 
 @Service
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
 
-    private final PostRepository postRepository;
-    private final ChatRoomRepository chatRoomRepository;
-    private final UserChatRoomsRepository userChatRoomsRepository;
-    private final HashTagRepository hashTagRepository;
-    private final BoardTagMapRepository boardTagMapRepository;
+	private final PostRepository postRepository;
+	private final ChatRoomRepository chatRoomRepository;
+	private final UserChatRoomsRepository userChatRoomsRepository;
+	private final HashTagRepository hashTagRepository;
+	private final BoardTagMapRepository boardTagMapRepository;
 
-    // 게시글 작성
-    @Override
-    @Transactional
-    public PostResponseDto createPost(PostRequestDto postRequestDto, User user) {
-        Post post = new Post(postRequestDto);
-        post.setUser(user);
-        postRepository.save(post);
+	// 게시글 작성
+	@Override
+	@Transactional
+	public PostResponseDto createPost(PostRequestDto postRequestDto, User user) {
+		Post post = new Post(postRequestDto);
+		post.setUser(user);
+		postRepository.save(post);
 
-        // 게시글 생성 과 함께 채팅방 개설
-        ChatRoom chatRoom = ChatRoom.builder()
-                .post(post)
-                .chatTitle(post.getTitle())
-                .activated(true)
-                .build();
+		// 게시글 생성 과 함께 채팅방 개설
+		ChatRoom chatRoom = ChatRoom.builder()
+			.post(post)
+			.chatTitle(post.getTitle())
+			.activated(true)
+			.build();
 
-        chatRoomRepository.save(chatRoom);
+		chatRoomRepository.save(chatRoom);
 
-        // UserChatRooms 생성
+		// UserChatRooms 생성
 
-        UserChatRoom userChatRoom = UserChatRoom.builder().user(user).chatRooms(chatRoom).role("ADMIN").build();
+		UserChatRoom userChatRoom = UserChatRoom.builder().user(user).chatRooms(chatRoom).role("ADMIN").build();
 
-        userChatRoomsRepository.save(userChatRoom);
+		userChatRoomsRepository.save(userChatRoom);
 
+		for (String tag : postRequestDto.getTags()) {
+			Tag newTag = hashTagRepository.findTagByContent(tag).orElse(null);
+			if (Objects.isNull(newTag)) {
+				//존재하지 않을 때
+				newTag = new Tag(tag);
+				hashTagRepository.save(newTag);
+				// 태그와 게시글의 중간 테이블 생성
+				BoardTagMap boardTagMap = new BoardTagMap(post, newTag);
+				boardTagMapRepository.save(boardTagMap);
 
-        for (String tag : postRequestDto.getTags()) {
-            Tag newTag = hashTagRepository.findTagByContent(tag).orElse(null);
-            if (Objects.isNull(newTag)) {
-                //존재하지 않을 때
-                newTag = new Tag(tag);
-                hashTagRepository.save(newTag);
-                // 태그와 게시글의 중간 테이블 생성
-                BoardTagMap boardTagMap = new BoardTagMap(post, newTag);
-                boardTagMapRepository.save(boardTagMap);
+			} else {
+				//존재할 때
+				BoardTagMap boardTagMap = new BoardTagMap(post, newTag);
+				boardTagMapRepository.save(boardTagMap);
+			}
+		}
 
-            } else {
-                //존재할 때
-                BoardTagMap boardTagMap = new BoardTagMap(post, newTag);
-                boardTagMapRepository.save(boardTagMap);
-            }
-        }
+		return new PostResponseDto(post);
+	}
 
-        return new PostResponseDto(post);
-    }
+	// 게시글 전체 조회
+	@Override
+	@Transactional(readOnly = true)
+	public Page<PostResponseDto> getPosts(Pageable pageable) {
+		Page<Post> posts = postRepository.findAllByOrderByCreatedAtDesc(pageable);
+		return posts.map(PostResponseDto::new);
+	}
 
-    // 게시글 전체 조회
-    @Override
-    @Transactional(readOnly = true)
-    public Page<PostResponseDto> getPosts(Pageable pageable) {
-        Page<Post> posts = postRepository.findAllByOrderByCreatedAtDesc(pageable);
-        return posts.map(PostResponseDto::new);
-    }
+	// 게시글 상세 조회
+	@Transactional
+	public PostResponseDto getPost(Long id) {
+		Post post = findPost(id);
+		return new PostResponseDto(post);
+	}
 
-    // 게시글 상세 조회
-    @Transactional
-    public PostResponseDto getPost(Long id) {
-        Post post = findPost(id);
-        return new PostResponseDto(post);
-    }
+	@Transactional
+	public void deleteOldTagMap(Long id) {
+		boardTagMapRepository.deleteAllByPost_Id(id);
+	}
 
-    @Transactional
-    public void deleteOldTagMap(Long id) {
-        boardTagMapRepository.deleteAllByPost_Id(id);
-    }
+	// 게시글 수정
+	@Transactional
+	@Override
+	public PostResponseDto updatePost(Long id, PostRequestDto postRequestDto, String username) {
+		Post post = findPost(id);
+		if (!post.getUser().getUsername().equals(username)) {
+			throw new RejectedExecutionException("작성자만 수정 가능합니다.");
+		}
 
-    // 게시글 수정
-    @Transactional
-    @Override
-    public PostResponseDto updatePost(Long id, PostRequestDto postRequestDto, String username) {
-        Post post = findPost(id);
-        if (!post.getUser().getUsername().equals(username)) {
-            throw new RejectedExecutionException("작성자만 수정 가능합니다.");
-        }
+		String modifiedTitle = postRequestDto.getTitle();
+		if (modifiedTitle != null) { //제목 수정되면 채팅방 이름 수정
+			ChatRoom chatRoom = chatRoomRepository.findByPostId(id).orElseThrow();
+			chatRoom.updateTitle(modifiedTitle);
+		}
+		// 기존 거 비우기
+		//        boardTagMapRepository.deleteAllByPost_Id(id);
+		// 입력한 태그 명 리스트 -> 이름으로 찾아서 존재하면
+		for (String tag : postRequestDto.getTags()) {
+			Tag newTag = hashTagRepository.findTagByContent(tag).orElse(null);
+			if (Objects.isNull(newTag)) {
+				//존재하지 않을 때
+				newTag = new Tag(tag);
+				hashTagRepository.save(newTag);
+				// 태그와 게시글의 중간 테이블 생성
+				BoardTagMap boardTagMap = new BoardTagMap(post, newTag);
+				boardTagMapRepository.save(boardTagMap);
 
-        String modifiedTitle = postRequestDto.getTitle();
-        if (modifiedTitle != null) { //제목 수정되면 채팅방 이름 수정
-            ChatRoom chatRoom = chatRoomRepository.findByPostId(id).orElseThrow();
-            chatRoom.updateTitle(modifiedTitle);
-        }
-        // 기존 거 비우기
-//        boardTagMapRepository.deleteAllByPost_Id(id);
-        // 입력한 태그 명 리스트 -> 이름으로 찾아서 존재하면
-        for (String tag : postRequestDto.getTags()) {
-            Tag newTag = hashTagRepository.findTagByContent(tag).orElse(null);
-            if (Objects.isNull(newTag)) {
-                //존재하지 않을 때
-                newTag = new Tag(tag);
-                hashTagRepository.save(newTag);
-                // 태그와 게시글의 중간 테이블 생성
-                BoardTagMap boardTagMap = new BoardTagMap(post, newTag);
-                boardTagMapRepository.save(boardTagMap);
+			} else {
+				//존재할 때
+				BoardTagMap boardTagMap = new BoardTagMap(post, newTag);
+				boardTagMapRepository.save(boardTagMap);
+			}
+		}
+		post.update(postRequestDto);
 
-            } else {
-                //존재할 때
-                BoardTagMap boardTagMap = new BoardTagMap(post, newTag);
-                boardTagMapRepository.save(boardTagMap);
-            }
-        }
-        post.update(postRequestDto);
+		return new PostResponseDto(post);
+	}
 
-        return new PostResponseDto(post);
-    }
+	// 게시글 삭제
+	@Override
+	public void deletePost(Long id, User user) {
+		Post post = findPost(id);
+		if (!post.getUser().getId().equals(user.getId())) {
+			throw new RejectedExecutionException("작성자만 삭제 가능합니다.");
+		}
+		postRepository.delete(post);
+	}
 
-    // 게시글 삭제
-    @Override
-    public void deletePost(Long id, User user) {
-        Post post = findPost(id);
-        if (!post.getUser().getId().equals(user.getId())) {
-            throw new RejectedExecutionException("작성자만 삭제 가능합니다.");
-        }
-        postRepository.delete(post);
-    }
+	// 모집완료 -> 모집중으로 변경
+	@Override
+	@Transactional
+	public void activateRecruitment(Long id, User user) {
+		Post post = findPost(id);
 
-    // 모집완료 -> 모집중으로 변경
-    @Override
-    @Transactional
-    public void activateRecruitment(Long id, User user) {
-        Post post = findPost(id);
+		if (!post.getUser().getId().equals(user.getId())) {
+			throw new RejectedExecutionException("작성자만 변경 가능합니다.");
+		}
 
-        if (!post.getUser().getId().equals(user.getId())) {
-            throw new RejectedExecutionException("작성자만 변경 가능합니다.");
-        }
+		if (post.getRecruitment()) {
+			throw new RecruitmentStatusException();
+		} else {
+			post.setRecruitment(true); // 저장 안 됨
+			System.out.println(post.getRecruitment());
+		}
+	}
 
-        if (post.getRecruitment()) {
-            throw new RecruitmentStatusException();
-        } else {
-            post.setRecruitment(true); // 저장 안 됨
-            System.out.println(post.getRecruitment());
-        }
-    }
+	// 모집중 -> 모집완료로 변경
+	@Override
+	@Transactional
+	public void completeRecruitment(Long id, User user) {
+		Post post = findPost(id);
 
-    // 모집중 -> 모집완료로 변경
-    @Override
-    @Transactional
-    public void completeRecruitment(Long id, User user) {
-        Post post = findPost(id);
+		if (!post.getUser().getId().equals(user.getId())) {
+			throw new RejectedExecutionException("작성자만 변경 가능합니다.");
+		}
 
-        if (!post.getUser().getId().equals(user.getId())) {
-            throw new RejectedExecutionException("작성자만 변경 가능합니다.");
-        }
+		if (post.getRecruitment()) {
+			post.setRecruitment(false); // 저장 안 됨
+		} else {
+			throw new RecruitmentStatusException();
+		}
+	}
 
-        if (post.getRecruitment()) {
-            post.setRecruitment(false); // 저장 안 됨
-        } else {
-            throw new RecruitmentStatusException();
-        }
-    }
+	@Override
+	@Transactional(readOnly = true)
+	public Page<PostResponseDto> searchPosts(String keyword, String criteria, boolean recruitment, Pageable pageable) {
+		Predicate predicate = PostExpressions.createBooleanExpression(keyword, criteria, recruitment);
+		Page<Post> postPage = postRepository.findAllByOrderByIdDesc(predicate, pageable);
 
-    @Override
-    @Transactional(readOnly = true)
-    public Page<PostResponseDto> searchPosts(String keyword, String criteria, boolean recruitment,
-                                             Pageable pageable) {
-        Page<Post> searchResultPage;
-        if (recruitment) {
-            searchResultPage = switch (criteria) {
-                case "username" ->
-                        postRepository.findByUser_UsernameContainingAndRecruitmentIsTrueOrderByCreatedAtDesc(keyword,
-                                pageable);
-                case "content" ->
-                        postRepository.findByContentContainingAndRecruitmentIsTrueOrderByCreatedAtDesc(keyword, pageable);
-                default ->
-                        postRepository.findByTitleContainingAndRecruitmentIsTrueOrderByCreatedAtDesc(keyword, pageable);
-            };
-        } else {
-            searchResultPage = switch (criteria) {
-                case "username" -> postRepository.findByUser_UsernameContainingOrderByCreatedAtDesc(keyword, pageable);
-                case "content" -> postRepository.findByContentContainingOrderByCreatedAtDesc(keyword, pageable);
-                default -> postRepository.findByTitleContainingOrderByCreatedAtDesc(keyword, pageable);
-            };
-        }
+		// Page<Post>를 Page<PostResponseDto>로 변환
+		return postPage.map(PostResponseDto::new);
+	}
 
-        return searchResultPage.map(PostResponseDto::new);
-    }
+	// @Override
+	// @Transactional(readOnly = true)
+	// public Page<PostResponseDto> searchPosts(String keyword, String criteria, boolean recruitment,
+	//                                          Pageable pageable) {
+	//     Page<Post> searchResultPage;
+	//     if (recruitment) {
+	//         searchResultPage = switch (criteria) {
+	//             case "username" ->
+	//                     postRepository.findByUser_UsernameContainingAndRecruitmentIsTrueOrderByCreatedAtDesc(keyword,
+	//                             pageable);
+	//             case "content" ->
+	//                     postRepository.findByContentContainingAndRecruitmentIsTrueOrderByCreatedAtDesc(keyword, pageable);
+	//             default ->
+	//                     postRepository.findByTitleContainingAndRecruitmentIsTrueOrderByCreatedAtDesc(keyword, pageable);
+	//         };
+	//     } else {
+	//         searchResultPage = switch (criteria) {
+	//             case "username" -> postRepository.findByUser_UsernameContainingOrderByCreatedAtDesc(keyword, pageable);
+	//             case "content" -> postRepository.findByContentContainingOrderByCreatedAtDesc(keyword, pageable);
+	//             default -> postRepository.findByTitleContainingOrderByCreatedAtDesc(keyword, pageable);
+	//         };
+	//     }
+	//
+	//     return searchResultPage.map(PostResponseDto::new);
+	// }
 
-    // 게시글 찾기
-    @Override
-    public Post findPost(Long id) {
-        return postRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-    }
+	// 게시글 찾기
+	@Override
+	public Post findPost(Long id) {
+		return postRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+	}
 
 }
