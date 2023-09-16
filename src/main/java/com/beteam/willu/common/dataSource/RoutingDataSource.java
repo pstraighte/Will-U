10f5ed505d58f099999dfc971acbcf401eb190a9
HyungCheolSim <sim34122@gmail.com>
2023-09-16 16:45:59 +0900

    replica test

---
+++ src/main/java/com/beteam/willu/common/dataSource/RoutingDataSource.java
@@ -0,0 +1,12 @@
+package com.beteam.willu.common.dataSource;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+public class RoutingDataSource extends AbstractRoutingDataSource {
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return TransactionSynchronizationManager.isCurrentTransactionReadOnly()
+                ? DataSourceType.SLAVE : DataSourceType.MASTER;
+    }
+}
